54eb6c64ba15235b23b86d13c42cb4076a0065bc
fjf3997 <1763994902@qq.com>
2020-04-14 20:07:14 +0800

    来自笔记本

--- src/com/leecode/MoveZeroes_283.java
+++ src/com/leecode/MoveZeroes_283.java
@@ -36,11 +36,22 @@ public class MoveZeroes_283 {
     空间复杂度: O(1)
      */
     public void moveZeroes2(int[] nums) {
-        int k=0;
+        /*int k=0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]!=0)
                 swap(nums,k++,i);
-        }
+        }*/
+        int k = 0; // nums中, [0...k)的元素均为非0元素
+
+        // 遍历到第i个元素后,保证[0...i]中所有非0元素
+        // 都按照顺序排列在[0...k)中
+        // 同时, [k...i] 为 0
+        for(int i = 0 ; i < nums.length ; i ++)
+            if(nums[i] != 0)
+                if(k != i)
+                    swap(nums, k++, i);
+                else
+                    k ++;
     }
     private void swap(int []nums,int i,int j){
         int t=nums[i];
---
+++ src/com/leecode/RemoveDuplicates_26.java
@@ -0,0 +1,41 @@
+package com.leecode;
+
+/**
+ * 给定一个排序数组，你需要在 原地 删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+ *
+ * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+ * 示例:
+ *给定数组 nums = [1,1,2],
+ *
+ * 函数应该返回新的长度 2, 并且原数组 nums 的前两个元素被修改为 1, 2。
+ *
+ * 你不需要考虑数组中超出新长度后面的元素。
+ *
+ */
+public class RemoveDuplicates_26 {
+    public int removeDuplicates(int[] nums) {
+        int k=1;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]!=nums[k-1]){
+                swap(nums,k++,i);
+            }
+        }
+        return k;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
+    }
+
+    public static void main(String[] args) {
+        int [] nums = {0,0,1,1,1,2,2,3,3,4};
+        System.out.println(new RemoveDuplicates_26().removeDuplicates(nums));
+        for (int num : nums) {
+            System.out.print(num+" ");
+
+        }
+
+    }
+}
