b61850816697a440d747fe985e9b994fbad0ade9
fjf3997 <1763994902@qq.com>
2020-05-04 12:04:59 +0800

    来自笔记本

--- src/com/leecode/LongestCommonSubsequence_1143.java
+++ src/com/leecode/LongestCommonSubsequence_1143.java
@@ -38,13 +38,28 @@ public class LongestCommonSubsequence_1143 {
         s2 = text2;
         int n = text1.length();
         int m = text2.length();
+        //动态规划
         if(n==0||m==0)
             return 0;
-        memo = new int [n][m];
+        memo = new int [n+1][m+1];
         for(int i=0;i<n;i++){
+            Arrays.fill(memo[i],0);
+        }
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(text1.charAt(i-1)==text2.charAt(j-1))
+                    memo[i][j]=1+memo[i-1][j-1];
+                else{
+                    memo[i][j]=Math.max(memo[i][j-1],memo[i-1][j]);
+                }
+            }
+        }
+        return memo[n][m];
+        //记忆搜索法
+        /*for(int i=0;i<n;i++){
             Arrays.fill(memo[i],-1);
         }
-        return lcs(n-1,m-1);
+        return lcs(n-1,m-1);*/
     }
 
     private int lcs(int n, int m) {
