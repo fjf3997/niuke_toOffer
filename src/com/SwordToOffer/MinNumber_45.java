7513bb39e7315a5b67a7ecad089ca4e95a45f575
fjf3997 <1763994902@qq.com>
2020-05-21 16:21:30 +0800

    来自笔记本

---
+++ src/com/SwordToOffer/MinNumber_45.java
@@ -0,0 +1,35 @@
+package com.SwordToOffer;
+
+import java.util.Arrays;
+
+/**
+ * 输入一个正整数数组，把数组里所有数字拼接起来排成一个数，打印能拼接出的所有数字中最小的一个。
+ * 示例 1:
+ * 输入: [10,2]
+ * 输出: "102"
+ * 示例 2:
+ * 输入: [3,30,34,5,9]
+ * 输出: "3033459"
+ * 提示:
+ * 0 < nums.length <= 100
+ * 说明:
+ * 输出结果可能非常大，所以你需要返回一个字符串而不是整数
+ * 拼接起来的数字可能会有前导 0，最后结果不需要去掉前导 0
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/ba-shu-zu-pai-cheng-zui-xiao-de-shu-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class MinNumber_45 {
+    public String minNumber(int[] nums) {
+        String [] strs = new String[nums.length];
+        for(int i=0;i<nums.length;i++){
+            strs[i] = String.valueOf(nums[i]);
+        }
+        Arrays.sort(strs,(x,y)->(x+y).compareTo(y+x));
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<strs.length;i++){
+            sb.append(strs[i]);
+        }
+        return sb.toString();
+    }
+}
